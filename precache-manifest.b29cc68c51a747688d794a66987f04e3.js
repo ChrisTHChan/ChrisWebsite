1af098d97836a6e4f9769927a9f04687f08b67e4
mrchrischan <chanchris1117@gmail.com>
2019-09-18 17:55:32 -0400

    Updates

---
+++ precache-manifest.b29cc68c51a747688d794a66987f04e3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e903dd7d6f51fc2c6719368cd8df1035",
+    "url": "/ChrisChan/index.html"
+  },
+  {
+    "revision": "0f216ac9a4f64f22db72",
+    "url": "/ChrisChan/static/css/main.06fcf79d.chunk.css"
+  },
+  {
+    "revision": "38e82f6ebd15cdb939a7",
+    "url": "/ChrisChan/static/js/2.8b0f7565.chunk.js"
+  },
+  {
+    "revision": "0f216ac9a4f64f22db72",
+    "url": "/ChrisChan/static/js/main.47ed4ed8.chunk.js"
+  },
+  {
+    "revision": "70d9c676cc2c9bc69cab",
+    "url": "/ChrisChan/static/js/runtime~main.3282b399.js"
+  },
+  {
+    "revision": "ca7160ef54f8e80aa46525a5d47663d4",
+    "url": "/ChrisChan/static/media/background.ca7160ef.png"
+  },
+  {
+    "revision": "4848c836b9141c63e713811e472ed676",
+    "url": "/ChrisChan/static/media/background1.4848c836.jpeg"
+  },
+  {
+    "revision": "81f47b244822645cb9193540d28d3a1a",
+    "url": "/ChrisChan/static/media/background2.81f47b24.jpeg"
+  },
+  {
+    "revision": "642767f0c791068468be7ddbdc304950",
+    "url": "/ChrisChan/static/media/classroom.642767f0.jpg"
+  },
+  {
+    "revision": "c937fa1cfb71833e15625c047c9c045f",
+    "url": "/ChrisChan/static/media/coding.c937fa1c.jpeg"
+  },
+  {
+    "revision": "c35e8b85f6575e1ad8d3220298420056",
+    "url": "/ChrisChan/static/media/house.c35e8b85.jpg"
+  },
+  {
+    "revision": "10a0b13599882cce5e2a6d88288160f5",
+    "url": "/ChrisChan/static/media/projects.10a0b135.jpg"
+  },
+  {
+    "revision": "54469eb3d2f5a334fde6c001b07d5bcc",
+    "url": "/ChrisChan/static/media/stocks.54469eb3.jpg"
+  },
+  {
+    "revision": "00446a5d6e661cb01f5b159071ec778c",
+    "url": "/ChrisChan/static/media/writing.00446a5d.jpg"
+  }
+]);
