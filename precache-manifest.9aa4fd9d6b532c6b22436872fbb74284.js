4f565541b4ae67a9a1d01c2b755bf55e3d4796ba
mrchrischan <chanchris1117@gmail.com>
2019-12-27 21:10:49 -0500

    Updates

---
+++ precache-manifest.9aa4fd9d6b532c6b22436872fbb74284.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fdde8fc3dff4527c694a52c899f8b4",
+    "url": "/ChrisWebsite/index.html"
+  },
+  {
+    "revision": "1bca1192a3fa1f87b7e1",
+    "url": "/ChrisWebsite/static/css/main.ddd2cc19.chunk.css"
+  },
+  {
+    "revision": "37beea88b6a3dbec8e51",
+    "url": "/ChrisWebsite/static/js/2.78c441b4.chunk.js"
+  },
+  {
+    "revision": "1bca1192a3fa1f87b7e1",
+    "url": "/ChrisWebsite/static/js/main.abcb144e.chunk.js"
+  },
+  {
+    "revision": "3e2b89a50082ee808a44",
+    "url": "/ChrisWebsite/static/js/runtime~main.b85534ed.js"
+  },
+  {
+    "revision": "a9c08353df99761d4aaa9d83157f92ce",
+    "url": "/ChrisWebsite/static/media/C.a9c08353.png"
+  },
+  {
+    "revision": "ca7160ef54f8e80aa46525a5d47663d4",
+    "url": "/ChrisWebsite/static/media/background.ca7160ef.png"
+  },
+  {
+    "revision": "4848c836b9141c63e713811e472ed676",
+    "url": "/ChrisWebsite/static/media/background1.4848c836.jpeg"
+  },
+  {
+    "revision": "81f47b244822645cb9193540d28d3a1a",
+    "url": "/ChrisWebsite/static/media/background2.81f47b24.jpeg"
+  },
+  {
+    "revision": "15a82af14bfb4f647fb21faa6e02c914",
+    "url": "/ChrisWebsite/static/media/background3.15a82af1.jpg"
+  },
+  {
+    "revision": "9d0126173588cb93df137a3afb6ed8bc",
+    "url": "/ChrisWebsite/static/media/background4.9d012617.jpg"
+  },
+  {
+    "revision": "c25e74f4132472d2849f6ed0fee24248",
+    "url": "/ChrisWebsite/static/media/background5.c25e74f4.jpg"
+  },
+  {
+    "revision": "825513f58b5ff59e68c1e429114b8010",
+    "url": "/ChrisWebsite/static/media/background6.825513f5.jpg"
+  },
+  {
+    "revision": "642767f0c791068468be7ddbdc304950",
+    "url": "/ChrisWebsite/static/media/classroom.642767f0.jpg"
+  },
+  {
+    "revision": "c937fa1cfb71833e15625c047c9c045f",
+    "url": "/ChrisWebsite/static/media/coding.c937fa1c.jpeg"
+  },
+  {
+    "revision": "3efc5675a5c1eccf39d0f556e63c04c5",
+    "url": "/ChrisWebsite/static/media/contact.3efc5675.jpg"
+  },
+  {
+    "revision": "c35e8b85f6575e1ad8d3220298420056",
+    "url": "/ChrisWebsite/static/media/house.c35e8b85.jpg"
+  },
+  {
+    "revision": "c36956dac7d98f203364c3bf01dd5bf7",
+    "url": "/ChrisWebsite/static/media/message.c36956da.jpg"
+  },
+  {
+    "revision": "10a0b13599882cce5e2a6d88288160f5",
+    "url": "/ChrisWebsite/static/media/projects.10a0b135.jpg"
+  },
+  {
+    "revision": "54469eb3d2f5a334fde6c001b07d5bcc",
+    "url": "/ChrisWebsite/static/media/stocks.54469eb3.jpg"
+  },
+  {
+    "revision": "00446a5d6e661cb01f5b159071ec778c",
+    "url": "/ChrisWebsite/static/media/writing.00446a5d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ChrisWebsite/precache-manifest.e8a90eabe0fc3576ef97548a77f17dd1.js"
+  "/ChrisWebsite/precache-manifest.9aa4fd9d6b532c6b22436872fbb74284.js"
 );
 
 self.addEventListener('message', (event) => {
